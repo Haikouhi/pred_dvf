c05b310e0301e5775311602a92b054acbc5d4c06
theoflaus <flaus.theo69@gmail.com>
2019-06-12 15:50:08 +0200

    modif API

--- pred_api.py
+++ pred_api.py
@@ -1,21 +1,26 @@
 from flask import request
 from flask_api import FlaskAPI
-from math import pi
+from Immo_model import *
 
 app = FlaskAPI(__name__)
+
+modele = Immo_model('valeurs-foncieres2017.csv', 'valeurs-foncieres2018.csv')
+modele.classification_tree_model()
+modele.regression_tree_model()
+modele.predict_cluster()
 
 
 @app.route("/", methods=['GET', 'POST'])
 def test():
-    lat = float(request.args['lattiude'])
+    lat = float(request.args['latitude'])
     longi = float(request.args['longitude'])
-    surface = float(request.args['surface_reelle_bati'])
-    pieces = float(request.args['nombre_pieces_principales'])
+    surface = float(request.args['superficie'])
+    pieces = float(request.args['nb_pieces'])
 
-    return {'': pi * rad**2}
+    res = modele.predict(surface, pieces, lat, longi)
+
+    return {'estimation': res}
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-test 
